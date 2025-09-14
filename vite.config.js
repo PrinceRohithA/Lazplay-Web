8a1b91cf6a733c51231dce253122f04680264961
PrinceRohithA <princerohith.a.2024.cse@rajalakshmi.edu.in>
2025-09-14 15:20:49 +0530

    delpoy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // default, but make sure it's set
+  },
+  server: {
+    port: 3000, // local dev server (optional)
+  },
+  base: '/', // 👈 important for custom domains
 })
